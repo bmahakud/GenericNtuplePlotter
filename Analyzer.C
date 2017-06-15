2a27c316fddf3e54665d4edd8623d131578e9a8a
bmahakud <bibhu.phy@gmail.com>
2017-06-15 08:22:33 -0500

    Code updated

--- Analyzer.C
+++ Analyzer.C
@@ -5,11 +5,14 @@
 #include "TLorentzVector.h"
 #include<vector>
 #include "TTree.h"
-#include "ClassReadSig.cc" 
- #include "ClassReadBkg.cc" 
- #include "ClassReadData.cc" 
+#include "ClassReadSig.cc"
+#include "ClassReadBkg.cc"
+#include "ClassReadData.cc" 
 #include "Loader.C"
- #include "HistCreater.C" 
+ #include "Looper_sig.C"
+#include "Looper_bkg.C" 
+#include "Looper_data.C"
+  #include "HistCreater.C"
 using namespace std;
 
 
@@ -18,24 +21,28 @@ void Analyzer(){
 Loader L;
 
 
-HistCreater hs;
 
-cout<<"Entries: "<<hs.h_HT_T5qqqqZH_mG750->GetEntries()<<endl;
-//All the histograms are already created , you can access them with hs.h_variableName_processName as shown above
+TFile *f=new TFile("Histograms.root","RECREATE");
+HistCreater hs;
+Looper_sig<ClassReadSig,HistCreater> loop_s;
+
 
 
 TChain *tree_T5qqqqZH_mG750=L.Load("T5qqqqZH_mG750");
 ClassReadSig T5qqqqZH_mG750(tree_T5qqqqZH_mG750);
+loop_s.Loop(tree_T5qqqqZH_mG750,T5qqqqZH_mG750,hs,"T5qqqqZH_mG750");
 
 
 
 TChain *tree_T5qqqqZH_mG2100=L.Load("T5qqqqZH_mG2100");
 ClassReadSig T5qqqqZH_mG2100(tree_T5qqqqZH_mG2100);
+loop_s.Loop(tree_T5qqqqZH_mG2100,T5qqqqZH_mG2100,hs,"T5qqqqZH_mG2100");
 
 
 
 TChain *tree_T5qqqqZH_mG1700=L.Load("T5qqqqZH_mG1700");
 ClassReadSig T5qqqqZH_mG1700(tree_T5qqqqZH_mG1700);
+loop_s.Loop(tree_T5qqqqZH_mG1700,T5qqqqZH_mG1700,hs,"T5qqqqZH_mG1700");
 
 
 
@@ -43,23 +50,28 @@ ClassReadSig T5qqqqZH_mG1700(tree_T5qqqqZH_mG1700);
 
 
 
+Looper_bkg<ClassReadBkg,HistCreater> loop_b;
 TChain *tree_QCD=L.Load("QCD");
 ClassReadBkg QCD(tree_QCD);
+loop_b.Loop(tree_QCD,QCD,hs,"QCD");
 
 
 
 TChain *tree_WJets=L.Load("WJets");
 ClassReadBkg WJets(tree_WJets);
+loop_b.Loop(tree_WJets,WJets,hs,"WJets");
 
 
 
 TChain *tree_TTJets=L.Load("TTJets");
 ClassReadBkg TTJets(tree_TTJets);
+loop_b.Loop(tree_TTJets,TTJets,hs,"TTJets");
 
 
 
 TChain *tree_ZJets=L.Load("ZJets");
 ClassReadBkg ZJets(tree_ZJets);
+loop_b.Loop(tree_ZJets,ZJets,hs,"ZJets");
 
 
 
@@ -67,15 +79,21 @@ ClassReadBkg ZJets(tree_ZJets);
 
 
 
+Looper_data<ClassReadData,HistCreater> loop_d;
 TChain *tree_HTMHT=L.Load("HTMHT");
 ClassReadData HTMHT(tree_HTMHT);
+loop_d.Loop(tree_HTMHT,HTMHT,hs,"HTMHT");
 
 
 
 TChain *tree_jetHT=L.Load("jetHT");
 ClassReadData jetHT(tree_jetHT);
+loop_d.Loop(tree_jetHT,jetHT,hs,"jetHT");
 
 
 
+
+
+f->Write();
 
 }
---
+++ Looper_bkg.C
@@ -0,0 +1,139 @@
+template <class classInst, class HistogramCreater> class Looper_bkg{
+public:
+
+void Loop(TChain *tr,classInst& read,HistogramCreater hs, string ProcessName);
+
+bool Baseline(TChain *tr,classInst& read,string ProcessName,int iEvent);
+
+bool ProcessCuts(TChain *tr,classInst& read,string ProcessName,int iEvent);
+
+bool OneTag(TChain *tr,classInst& read,string ProcessName,int iEvent);
+
+bool TwoTag(TChain *tr,classInst& read,string ProcessName,int iEvent);
+
+};
+
+template <class classInst,class HistogramCreater> void Looper_bkg<classInst,HistogramCreater>::Loop(TChain *tr,classInst& read,HistogramCreater hs,string ProcessName)
+
+{//looper definition
+
+string processes[9]={ "T5qqqqZH_mG750"  , "T5qqqqZH_mG2100"  , "T5qqqqZH_mG1700"  , "QCD"  , "WJets"  , "TTJets"  , "ZJets"  , "HTMHT"  , "jetHT"  };
+string dataprocesses[2]={ "HTMHT"  , "jetHT"  };
+const int processesSize=9;
+            auto it=std::find(processes,processes+processesSize,ProcessName);
+            int processInt=it-processes;
+       std::cout<<"Total number of entries: "<<tr->GetEntries()<<std::endl;
+
+       for(int iEv=0;iEv<tr->GetEntries();iEv++){// event loop
+
+       tr->GetEntry(iEv);
+
+          if(iEv % 1000==0)std::cout<<"Running on "<<ProcessName<<" event no: "<<iEv<<"---completed : "<<(float(iEv)/float(tr->GetEntries()))*100<<" %"<<std::endl;
+
+            if(Baseline(tr,read,ProcessName,iEv)  && ProcessCuts(tr,read,ProcessName, iEv)){ //Baseline for this processn
+
+            double EventWeight=1;
+            EventWeight=read.Weight;
+if(OneTag(tr,read,ProcessName,iEv)){
+         hs.h_[0][processInt][0]->Fill(read.HT,EventWeight); 
+         hs.h_[1][processInt][0]->Fill(read.MHT,EventWeight); 
+         hs.h_[2][processInt][0]->Fill(read.NJets,EventWeight); 
+         hs.h_[3][processInt][0]->Fill(read.BTags,EventWeight); 
+         hs.h_[4][processInt][0]->Fill(read.DeltaPhi1,EventWeight); 
+         hs.h_[5][processInt][0]->Fill(read.Jets->at(0).Pt(),EventWeight); 
+}
+
+if(TwoTag(tr,read,ProcessName,iEv)){
+         hs.h_[0][processInt][1]->Fill(read.HT,EventWeight); 
+         hs.h_[1][processInt][1]->Fill(read.MHT,EventWeight); 
+         hs.h_[2][processInt][1]->Fill(read.NJets,EventWeight); 
+         hs.h_[3][processInt][1]->Fill(read.BTags,EventWeight); 
+         hs.h_[4][processInt][1]->Fill(read.DeltaPhi1,EventWeight); 
+         hs.h_[5][processInt][1]->Fill(read.Jets->at(0).Pt(),EventWeight); 
+}
+
+               }//Baseline for this process
+
+       }//event loop
+
+}//looper definition
+
+template <class classInst,class HistogramCreater> bool Looper_bkg<classInst,HistogramCreater>::Baseline(TChain *tr,classInst& read,string ProcessName,int iEvent){
+
+if(
+read. HT>500 && 
+read. MHT>200 && 
+read. NJets>=4 && 
+read. Jets->size()>0 && 
+read. Jets->at(0).Pt() > 200.
+){
+
+return true;
+
+}
+else{
+return false;
+
+}
+}
+
+template <class classInst,class HistogramCreater> bool Looper_bkg<classInst,HistogramCreater>::ProcessCuts(TChain *tr,classInst& read,string ProcessName,int iEvent){
+
+if(ProcessName=="T5qqqqZH_mG750"){
+
+return true;
+}
+
+if(ProcessName=="T5qqqqZH_mG1700"){
+
+return true;
+}
+
+if(ProcessName=="T5qqqqZH_mG2100"){
+
+return true;
+}
+
+if(ProcessName=="WJets"){
+
+return true;
+}
+
+if(ProcessName=="ZJets"){
+
+return true;
+}
+
+if(ProcessName=="TTJets"){
+
+return true;
+}
+
+if(ProcessName=="QCD"){
+
+return true;
+}
+
+if(ProcessName=="HTMHT"){
+
+return true;
+}
+
+if(ProcessName=="jetHT"){
+
+return true;
+}
+
+return false;
+}
+
+template <class classInst,class HistogramCreater> bool Looper_bkg<classInst,HistogramCreater>::OneTag(TChain *tr,classInst& read,string ProcessName,int iEvent){
+
+return true;
+}
+
+template <class classInst,class HistogramCreater> bool Looper_bkg<classInst,HistogramCreater>::TwoTag(TChain *tr,classInst& read,string ProcessName,int iEvent){
+
+return true;
+}
+
